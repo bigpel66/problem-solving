e2fa641485efb2d7410662a6a1b110454c0c14ec
BIGPEL <bigpel66@icloud.com>
2023-05-17 18:07:01 +0900

    [unrated] Title: 배열 조각하기, Time: 4.28 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        for (int i = 0 ; i < query.length ; i++) {
+            if (list.size() < query[i]) {
+                continue;
+            }
+            if (i % 2 == 0) {
+                list = list.subList(0, query[i] + 1);
+            } else {
+                list = list.subList(query[i], list.size());
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
