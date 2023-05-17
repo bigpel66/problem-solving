79a52d5519ba2e2368e83440e1b3a0047405fe35
BIGPEL <bigpel66@icloud.com>
2023-05-17 21:12:07 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int angle) {
+        if (angle < 90) {
+            return 1;
+        } else if (angle == 90) {
+            return 2;
+        } else if (angle == 180) {
+            return 4;
+        } else {
+            return 3;
+        }
+    }
+}
