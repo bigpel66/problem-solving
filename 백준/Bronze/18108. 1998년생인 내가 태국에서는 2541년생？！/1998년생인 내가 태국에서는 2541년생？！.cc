f2a8922c6a0036758d9a34319f117ff325bb4b26
BIGPEL <bigpel66@icloud.com>
2023-05-17 16:45:03 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int year;
+    cin >> year;
+    cout << year - (2541 - 1998);
+}
