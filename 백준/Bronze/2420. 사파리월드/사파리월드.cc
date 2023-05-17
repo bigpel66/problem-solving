beb5aae1ec128afbcbcf7e17b2b70121aaefdbdb
BIGPEL <bigpel66@icloud.com>
2023-05-17 16:01:26 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    long long a, b;
+    cin >> a >> b;
+    cout << abs(b - a);
+    return 0;
+}
