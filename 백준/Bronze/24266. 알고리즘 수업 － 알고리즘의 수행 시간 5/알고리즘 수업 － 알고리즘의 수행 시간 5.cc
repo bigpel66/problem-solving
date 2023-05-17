be304ce77fab4e72aabd089f7ff1fb20777795c8
BIGPEL <bigpel66@icloud.com>
2023-05-17 16:48:05 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class sol {
+    private:
+        long long n;
+        
+        void stream(void) {
+            cin.tie(nullptr);
+            ios_base::sync_with_stdio(false);
+        }
+        
+        void get_input(void) {
+            cin >> n;
+        }
+        
+        void solve(void) {
+            cout << n * n * n << '\n' << 3;
+        }
+
+    public:
+        sol(void) {
+            stream();
+            get_input();
+            solve();
+        }
+};
+
+int main(void) {
+    sol ve;
+    return 0;
+}
