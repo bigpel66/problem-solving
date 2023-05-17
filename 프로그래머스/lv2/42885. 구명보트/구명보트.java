0d607f7d1f3bcb721bf9f91ef359c91b3b044316
BIGPEL <bigpel66@icloud.com>
2023-05-17 21:23:48 +0900

    [level 2] Title: 구명보트, Time: 10.01 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int answer = 0;
+        int left = 0;
+        int right = people.length - 1;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            answer++;
+        }
+        return answer;
+    }
+}
