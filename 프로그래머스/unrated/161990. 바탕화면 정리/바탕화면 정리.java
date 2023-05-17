6ac623e1ae8e15fbb04f3694a3389eabf922e5bf
BIGPEL <bigpel66@icloud.com>
2023-05-17 18:22:24 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for (int i = 0 ; i < wallpaper.length ; i++) {
+            for (int j = 0 ; j < wallpaper[i].length() ; j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    minX = Math.min(minX, i);
+                    minY = Math.min(minY, j);
+                    maxX = Math.max(maxX, i);
+                    maxY = Math.max(maxY, j);
+                }
+            }
+        }
+        return new int[]{minX, minY, maxX + 1, maxY + 1};
+    }
+}
