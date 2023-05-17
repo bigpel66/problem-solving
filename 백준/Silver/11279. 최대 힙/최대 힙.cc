a61fb2e70aebebbdd8e4514b09c9abe593f8619d
BIGPEL <bigpel66@icloud.com>
2023-05-17 16:37:24 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class sol {
+    private:
+        int n;
+        priority_queue<int> q;
+        
+        void stream(void) {
+            cin.tie(nullptr);
+            ios_base::sync_with_stdio(false);
+        }
+        
+        void get_input(void) {
+            cin >> n;
+        }
+        
+        void solve(void) {
+            int temp;
+            for (int i = 0 ; i < n ; i++) {
+                cin >> temp;
+                if (!temp) {
+                    if (q.empty()) {
+                        cout << 0 << '\n';
+                    } else {
+                        cout << q.top() << '\n';
+                        q.pop();
+                    }
+                } else {
+                    q.push(temp);
+                }
+            }
+        }
+
+    public:
+        sol(void) {
+            stream();
+            get_input();
+            solve();
+        }
+};
+
+int main(void) {
+    sol ve;
+    return 0;
+}
