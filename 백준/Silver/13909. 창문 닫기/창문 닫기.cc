57e526e82ba1ccad66fae93161bf831810e4aed8
BIGPEL <bigpel66@icloud.com>
2023-05-17 16:43:07 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+class sol {
+    private:
+        long long n;
+        
+        void stream(void) {
+            cin.tie(nullptr);
+            ios_base::sync_with_stdio(false);
+        }
+        
+        void get_input(void) {
+            cin >> n;
+        }
+        
+        void solve(void) {
+            cout << floor(sqrt(n));
+        }
+
+    public:
+        sol(void) {
+            stream();
+            get_input();
+            solve();
+        }
+};
+
+int main(void) {
+    sol ve;
+    return 0;
+}
