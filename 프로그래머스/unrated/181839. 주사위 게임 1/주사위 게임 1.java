7bad1f6f777d4b8d3dad575998bf9f846de7a908
BIGPEL <bigpel66@icloud.com>
2023-05-17 18:18:26 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b) {
+        if (a % 2 == 1 && b % 2 == 1) {
+            return (int) (Math.pow(a, 2) + Math.pow(b, 2));
+        } else if (a % 2 == 0 && b % 2 ==0) {
+            return Math.abs(a - b);
+        } else {
+            return (a + b) * 2;
+        }
+    }
+}
