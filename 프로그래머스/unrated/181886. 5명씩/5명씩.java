92d9235d80e076c4349ef57b5ddfb277e9b1f9c4
BIGPEL <bigpel66@icloud.com>
2023-05-17 18:11:09 +0900

    [unrated] Title: 5명씩, Time: 3.45 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+        return IntStream.range(0, names.length).filter(i -> i % 5 == 0).mapToObj(i -> names[i]).toArray(String[]::new);
+    }
+}
