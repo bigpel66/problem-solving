822da1fe6b0fe1b52c370d935b92dacc4e94f0fb
BIGPEL <bigpel66@icloud.com>
2023-05-17 18:18:10 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < str_list.length ; i++) {
+            if (str_list[i].contains(ex)) {
+                continue;
+            }
+            sb.append(str_list[i]);
+        }
+        return sb.toString();
+    }
+}
