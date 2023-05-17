51c354709cae9956cb6aa7f9dd69a2e91b0ac1a9
BIGPEL <bigpel66@icloud.com>
2023-05-17 18:27:37 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] common) {
+        if (isArithmetic(common)) {
+            return common[common.length - 1] + (common[1] - common[0]);
+        } else {
+            return common[common.length - 1] * (common[1] / common[0]);
+        }
+    }
+    
+    public boolean isArithmetic(int[] common) {
+        return common[1] - common[0] == common[2] - common[1];
+    }
+}
