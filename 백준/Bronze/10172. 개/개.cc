4fb08c067456afb885d407bb2f96581a3194dd16
BIGPEL <bigpel66@icloud.com>
2023-05-17 16:27:49 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "|\\_/|";
+    std::cout << std::endl;
+    std::cout << "|q p|   /}";
+    std::cout << std::endl;
+    std::cout << "( 0 )\"\"\"\\";
+    std::cout << std::endl;
+    std::cout << "|\"^\"`    |";
+    std::cout << std::endl;
+    std::cout << "||_/=\\\\__|";
+
+    return 0;
+}
