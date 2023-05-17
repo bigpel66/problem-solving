3d94c96ff2ce053e84fa2af5edd3d76df53d91dd
BIGPEL <bigpel66@icloud.com>
2023-05-17 16:01:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int value;
+
+    std::cin >> value;
+
+    for(int i = 0; i < value; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
