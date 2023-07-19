6f2ee137abad0ad15c70d8184f14a7ab814d7d1b
BIGPEL <bigpel66@icloud.com>
2023-07-20 07:29:18 +0900

    [Bronze V] Title: A+B, Time: 140 ms, Memory: 13440 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BigInteger i1, i2;
+
+    public Main() throws Exception {
+        getInput();
+        solve();
+    }
+
+    void getInput() throws IOException {
+        i1 = new BigInteger(br.readLine());
+        i2 = new BigInteger(br.readLine());
+    }
+
+    void solve() throws IOException {
+        bw.write(i1.add(i2).toString());
+        bw.flush();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
+    }
+}
