9c3c2db2f5ae760e0cf8137815bc601ed9b059cd
BIGPEL <bigpel66@icloud.com>
2023-08-28 21:00:50 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2320 ms, Memory: 439400 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuffer sb = new StringBuffer();
+    int numberOfNode, answer;
+    int [][] dp;
+    List<Integer>[] node;
+
+    public Main() throws Exception {
+        getInput();
+        solve();
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    void getInput() throws Exception {
+        numberOfNode = Integer.parseInt(br.readLine());
+        answer = Integer.MAX_VALUE;
+        dp = new int[numberOfNode + 1][2];
+        node = new ArrayList[numberOfNode + 1];
+        for (int i = 0; i <= numberOfNode; i++) {
+            node[i] = new ArrayList<>();
+            Arrays.fill(dp[i], -1);
+        }
+        for (int i = 0; i < numberOfNode - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            node[u].add(v);
+            node[v].add(u);
+        }
+    }
+
+    int dfs(int cur, int prev, int isSelected) {
+        if (dp[cur][isSelected] != -1) {
+            return dp[cur][isSelected];
+        }
+        dp[cur][isSelected] = isSelected == 1 ? 1 : 0;
+        for (int next : node[cur]) {
+            if (prev == next) {
+                continue;
+            }
+            if (isSelected == 0) {
+                dp[cur][isSelected] += dfs(next, cur, 1);
+            } else {
+                dp[cur][isSelected] += Math.min(dfs(next, cur, 0), dfs(next, cur, 1));
+            }
+        }
+        return dp[cur][isSelected];
+    }
+
+    void solve() {
+        sb.append(Math.min(dfs(1, -1, 1), dfs(1, -1, 0)));
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
+    }
+}
