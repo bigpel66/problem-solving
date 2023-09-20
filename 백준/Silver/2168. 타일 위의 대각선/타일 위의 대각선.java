e8fb6172e51da0c493ec7fbc1798e9b0c081bcb9
BIGPEL <bigpel66@icloud.com>
2023-09-20 22:46:56 +0900

    [Silver I] Title: 타일 위의 대각선, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Silver/2168. 타일 위의 대각선/타일 위의 대각선.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private final StringBuilder sb;
+    private int x, y;
+
+    public Main() throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            sb = new StringBuilder();
+            getInput(br);
+            solve();
+            bw.write(sb.toString());
+            bw.flush();
+        }
+    }
+
+    private void getInput(BufferedReader br) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    private void solve() {
+        sb.append(x + y - gcd(x, y));
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
+    }
+}
