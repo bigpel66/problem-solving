fe975df03484ca2e974e399714a4a1e891f3fba5
BIGPEL <bigpel66@icloud.com>
2023-09-21 07:28:18 +0900

    [Bronze V] Title: WARBOY, Time: 80 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private final StringBuilder sb;
+    private int x, y, z;
+
+    public Main() throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            sb = new StringBuilder();
+            getInput(br);
+            solve();
+            bw.write(sb.toString());
+            bw.flush();
+        }
+    }
+
+    private void getInput(BufferedReader br) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        z = Integer.parseInt(st.nextToken());
+    }
+
+    private void solve() {
+        sb.append(y / x * 3 * z);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
+    }
+}
