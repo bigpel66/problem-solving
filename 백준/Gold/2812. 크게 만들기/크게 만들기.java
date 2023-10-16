699da6165773c99d5e9bfd108a0e7b75e49f23b7
BIGPEL <bigpel66@icloud.com>
2023-10-16 20:54:56 +0900

    [Gold III] Title: 크게 만들기, Time: 400 ms, Memory: 41784 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public final class Main {
+    private final StringBuilder sb;
+    private int numberOfDigit, numberOfRemove;
+    private String number;
+    private Deque<Character> stack = new LinkedList<>();
+
+    public Main() throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            sb = new StringBuilder();
+            getInput(br);
+            solve();
+            bw.write(sb.toString());
+            bw.flush();
+        }
+    }
+
+    private void getInput(BufferedReader br) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        numberOfDigit = Integer.parseInt(st.nextToken());
+        numberOfRemove = Integer.parseInt(st.nextToken());
+        number = br.readLine();
+    }
+
+    private void solve() {
+        int limit = numberOfDigit - numberOfRemove;
+        for (int i = 0; i < number.length(); i++) {
+            while (!stack.isEmpty() && numberOfRemove > 0 && stack.peekLast() < number.charAt(i)) {
+                stack.pollLast();
+                numberOfRemove--;
+            }
+            stack.offerLast(number.charAt(i));
+        }
+        for (int i = 0; i < limit; i++) {
+            sb.append(stack.pollFirst());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
+    }
+
+    public static final class Town implements Comparable<Town> {
+        public int position;
+        public int residence;
+
+        public Town(int position, int residence) {
+            this.position = position;
+            this.residence = residence;
+        }
+
+        @Override
+        public int compareTo(Town town) {
+            return Integer.compare(position, town.position);
+        }
+    }
+}
