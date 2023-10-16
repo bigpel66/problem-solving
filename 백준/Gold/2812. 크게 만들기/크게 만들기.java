2aeb4221bcf24ed88d572ac068a5422157503112
BIGPEL <bigpel66@icloud.com>
2023-10-16 20:55:51 +0900

    [Gold III] Title: 크게 만들기, Time: 384 ms, Memory: 41596 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public final class Main {
     private final StringBuilder sb;
