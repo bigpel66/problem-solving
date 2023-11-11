696cca5cb3cb1cfff2d3a3d36fd311e18dd35b35
BIGPEL <bigpel66@icloud.com>
2023-11-11 10:55:58 +0900

    [Gold IV] Title: A를 B로, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/13019. A를 B로/A를 B로.java
+++ 백준/Gold/13019. A를 B로/A를 B로.java
@@ -4,32 +4,60 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public final class Main {
-    public static void main(String[] args) throws Exception {
-         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String stringA = br.readLine();
-            String stringB = br.readLine();
-            int[] alpha = new int[26];
-            for (int i = 0; i < stringA.length(); ++i) {
-                ++alpha[stringA.charAt(i) - 'A'];
-                --alpha[stringB.charAt(i) - 'A'];
-            }
-            for (int i = 0; i < 26; i++) {
-                if (alpha[i] != 0) {
-                    System.out.println(-1);
-                    return;
-                }
-            }
-            int count = 0;
-            int offset = stringA.length() - 1;
-            int i = stringB.length() - 1;
-            for (; offset >= 0; --offset) {
-                if (stringB.charAt(i) != stringA.charAt(offset)) {
-                    count++;
-                } else {
-                    i--;
-                }
-            }
-            System.out.print(count);
+    private final StringBuilder sb;
+    private String stringA, stringB;
+
+    public Main() throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            sb = new StringBuilder();
+            getInput(br);
+            solve();
+            bw.write(sb.toString());
+            bw.flush();
         }
+    }
+
+    private void getInput(BufferedReader br) throws Exception {
+        stringA = br.readLine();
+        stringB = br.readLine();
+    }
+
+    private boolean isTwoInputDifferent() {
+        int[] alphabets = new int[26];
+        for (int i = 0; i < stringA.length(); i++) {
+            ++alphabets[stringA.charAt(i) - 'A'];
+            --alphabets[stringB.charAt(i) - 'A'];
+        }
+        for (int i = 0; i < 26; i++) {
+            if (alphabets[i] != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int findMoveCount() {
+        int count = 0;
+        for (int offset = stringB.length() - 1, i = stringA.length() - 1; i >= 0; i--) {
+            if (stringA.charAt(i) == stringB.charAt(offset)) {
+                offset--;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void solve() {
+        if (isTwoInputDifferent()) {
+            sb.append(-1);
+        } else {
+            sb.append(findMoveCount());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
     }
 }
