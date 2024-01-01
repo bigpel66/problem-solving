aeff29a0ac35fb84c73944490cebcfdf71396d30
BIGPEL <bigpel66@icloud.com>
2024-01-01 12:25:43 +0900

    [Bronze V] Title: CASIO, Time: 120 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public final class Main {
+    private final StringBuilder sb;
+    private String student, teacher;
+
+    public Main() throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            sb = new StringBuilder();
+            getInput(br);
+            solve();
+            bw.write(sb.toString());
+            bw.flush();
+        }
+    }
+
+    private void getInput(BufferedReader br) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        student = st.nextToken();
+        teacher = st.nextToken();
+    }
+
+    private void solve() {
+        sb.append(student.equals(teacher) ? 1 : 0);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
+    }
+}
