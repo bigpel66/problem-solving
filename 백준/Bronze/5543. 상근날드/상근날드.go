095bba4cba81850e59d6bc208a1bc8791b9c6bdb
BIGPEL <bigpel66@icloud.com>
2024-03-04 17:47:06 +0900

    [Bronze IV] Title: 상근날드, Time: 4 ms, Memory: 880 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Problem struct {
+	*bufio.Reader
+	*bufio.Writer
+	*StringBuilder
+	*Data
+}
+
+type Data struct {
+	values []int
+}
+
+func NewProblem() *Problem {
+	return &Problem{
+		Reader:        bufio.NewReader(os.Stdin),
+		Writer:        bufio.NewWriter(os.Stdout),
+		StringBuilder: NewStringBuilder(),
+		Data:          NewData(),
+	}
+}
+
+func NewData() *Data {
+	return &Data{}
+}
+
+func (p *Problem) Solve() {
+	p.input()
+	p.logic()
+	p.output()
+}
+
+func (p *Problem) input() {
+	for i := 0; i < 5; i++ {
+		var v int
+		if _, err := fmt.Fscan(p.Reader, &v); err != nil {
+			os.Exit(1)
+		}
+		p.values = append(p.values, v)
+	}
+}
+
+func (p *Problem) logic() {
+	ham := smallest(p.values[0:3])
+	bev := smallest(p.values[3:5])
+	p.Append(ham + bev - 50)
+}
+
+func smallest(values []int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func (p *Problem) output() {
+	_, err := fmt.Fprint(p.Writer, p.String())
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func main() {
+	p := NewProblem()
+	defer func(p *Problem) {
+		_ = p.Flush()
+	}(p)
+	p.Solve()
+}
+
+type StringBuilder struct {
+	strings.Builder
+}
+
+func NewStringBuilder() *StringBuilder {
+	var sb StringBuilder
+	sb.Grow(30)
+	return &sb
+}
+
+func (sb *StringBuilder) Append(values ...interface{}) *StringBuilder {
+	for _, value := range values {
+		sb.WriteString(convert(value))
+	}
+	return sb
+}
+
+func convert(value interface{}) string {
+	switch v := value.(type) {
+	case string:
+		return v
+	case int:
+		return strconv.Itoa(v)
+	case int8:
+		return strconv.Itoa(int(v))
+	case int16:
+		return strconv.Itoa(int(v))
+	case int32:
+		return strconv.Itoa(int(v))
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case uint:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		return strconv.FormatUint(v, 10)
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
