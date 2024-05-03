f3e659e9371dac4d0d0d3e2f5aef2ae4ed6719ef
BIGPEL <bigpel66@icloud.com>
2024-05-03 23:15:14 +0900

    [Bronze IV] Title: Quadrants, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9772. Quadrants/Quadrants.py
+++ 백준/Bronze/9772. Quadrants/Quadrants.py
@@ -1,9 +1,8 @@
 while True:
     X, Y = map(float, input().split())
-    if X == 0 and Y == 0:
-        print("AXIS")
-        break
     if X == 0 or Y == 0:
         print("AXIS")
     else:
         print("Q1" if X > 0 and Y > 0 else "Q3" if X < 0 and Y < 0 else "Q2" if X < 0 < Y else "Q4")
+    if X == Y == 0:
+        break
