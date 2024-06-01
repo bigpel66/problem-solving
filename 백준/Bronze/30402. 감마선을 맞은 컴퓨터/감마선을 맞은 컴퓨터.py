9328d4c2ac81dcc82b77175665be2b45ff707723
BIGPEL <bigpel66@icloud.com>
2024-06-02 02:06:19 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for i in range(15):
+    s = input()
+    if 'w' in s:
+        print('chunbae')
+        break
+    elif 'b' in s:
+        print('nabi')
+        break
+    elif 'g' in s:
+        print('yeongcheol')
+        break
