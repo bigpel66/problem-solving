8bc32ed21bddcd5e8c83c12d3d0286f4e5b3f541
BIGPEL <bigpel66@icloud.com>
2024-09-06 08:33:01 +0900

    [Bronze III] Title: 터널의 입구와 출구, Time: 276 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = int(input())
+max_p = m
+for _ in range(n):
+    a, b = map(int, input().split())
+    m = m + a - b
+    if m < 0:
+        max_p = 0
+        break
+    if max_p < m:
+        max_p = m
+print(max_p)
