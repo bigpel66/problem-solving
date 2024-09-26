ada4670b146ac05e875cbe35819f3bd091e5f1fa
BIGPEL <bigpel66@icloud.com>
2024-09-26 10:10:11 +0900

    [Bronze III] Title: 직각 삼각형의 두 변, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6322. 직각 삼각형의 두 변/직각 삼각형의 두 변.py
@@ -0,0 +1,17 @@
+i = 1
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == 0:
+        break
+    if i > 1:
+        print()
+    print(f"Triangle #{i}")
+    if c == -1:
+        print("c = %.3f" % ((a**2 + b**2) ** 0.5))
+    elif max(a, b) >= c:
+        print("Impossible.")
+    elif a == -1:
+        print("a = %.3f" % ((c**2 - b**2) ** 0.5))
+    elif b == -1:
+        print("b = %.3f" % ((c**2 - a**2) ** 0.5))
+    i += 1
