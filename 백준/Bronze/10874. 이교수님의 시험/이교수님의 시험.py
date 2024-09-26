8edd9fb35b1c7ba18fe7148a98225489364ff3ff
BIGPEL <bigpel66@icloud.com>
2024-09-26 10:10:00 +0900

    [Bronze III] Title: 이교수님의 시험, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10874. 이교수님의 시험/이교수님의 시험.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    tmp = list(map(int, input().split()))
+    cnt = 0
+    for i in range(10):
+        if tmp[i] == (i + 1) % 5: 
+            cnt += 1
+    if cnt == 8: 
+        print(_ + 1)
